4ed6866035286cc53eeb30a146f5e6d51f018f7b
LucaLJX <lis_2b@hotmail.com>
2017-11-29 21:12:10 +0800

    lucaljx
    store:
    初始化

--- dragonball/src/components/store/store.js
+++ dragonball/src/components/store/store.js
@@ -7,13 +7,17 @@ export default new Vuex.Store({
   state: {
     count: 0,
     // 判断当前是几星
-    starIndex: 2,
+    starIndex: 1,
     // 总答题数
     totalTime: 0,
     // 正确次数
     rightTime: 0,
     // 本次答题正确次数
-    thisRightTime: 0
+    thisRightTime: 0,
+    openid: '',
+    splitKey: '17d7ece13c634486a35f9600f18f6efd',
+    host: 'http://question.vigoo.cn/',
+    allFinish: false
   },
   mutations: {
     increment (state) {
