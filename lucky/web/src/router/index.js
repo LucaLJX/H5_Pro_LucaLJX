db43ddc175f0b1f278a8c874d7de10035cab9488
luca_ljx <443926598@qq.com>
2020-12-02 09:36:50 +0800

    init(抽奖): 初始化项目

---
+++ lucky/web/src/router/index.js
@@ -0,0 +1,43 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/luckDraw',
+    name: 'luckDraw',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/luck-draw'),
+  },
+  {
+    path: '/checkIn',
+    name: 'checkIn',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/check-in'),
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
---
+++ lucky/web/src/views/check-in/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    yyy
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: '123',
+    }
+  },
+  mounted() {
+    console.log('12')
+  },
+}
+</script>
